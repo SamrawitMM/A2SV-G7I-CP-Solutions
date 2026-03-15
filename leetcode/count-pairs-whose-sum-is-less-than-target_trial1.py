a6d3e2ca0ba3e95c5bed2cd30180332c76f230f2
Samrawit Mewa <37739754+SamrawitMM@users.noreply.github.com>
2026-03-15 23:59:51 +0300

    Add solution for Count Pairs Whose Sum is Less than Target (trial 1)

---
+++ leetcode/count-pairs-whose-sum-is-less-than-target_trial1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def countPairs(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+        count = 0
+        nums.sort()
+        while left < right:
+
+            if nums[left] + nums[right] < target:
+                count += right - left
+                left += 1
+            
+            else:
+                right -= 1
+
+        return count
+
+
+
+        
