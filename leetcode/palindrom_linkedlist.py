462403aca950e12f5934a8d80d3f6c5807588138
Samrawit Mewa <37739754+SamrawitMM@users.noreply.github.com>
2026-04-04 22:37:28 +0300

    Create palindrom_linkedlist.py

---
+++ leetcode/palindrom_linkedlist.py
@@ -0,0 +1,28 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        stack = []
+        current = head
+
+        while current:
+            stack.append(current.val)
+            current = current.next
+
+        
+        current = head
+        while current:
+            if current.val == stack.pop():
+                current = current.next
+
+            else:
+                return False
+
+        
+        return True
+            
+            
+        
