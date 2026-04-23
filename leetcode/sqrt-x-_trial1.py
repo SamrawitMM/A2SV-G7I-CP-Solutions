cbbe7ad791feb99d1786ea670147abfac4b1b5fd
Samrawit Mewa <37739754+SamrawitMM@users.noreply.github.com>
2026-04-23 13:47:11 +0300

    Add solution for Sqrt(x) (trial 1)

---
+++ leetcode/sqrt-x-_trial1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        left = 0
+        right = x
+
+        
+
+        while left < right:
+
+            mid = ( left + right + 1 ) // 2
+
+            if mid ** 2 <= x:
+                left = mid
+
+            else:
+
+                right = mid - 1
+
+
+        return int(left)
+        
