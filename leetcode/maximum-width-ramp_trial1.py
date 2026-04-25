88db4930f7538177a7578eabcd19e59707a73aa4
Samrawit Mewa <37739754+SamrawitMM@users.noreply.github.com>
2026-04-25 19:52:31 +0300

    Add solution for Maximum Width Ramp (trial 1)

---
+++ leetcode/maximum-width-ramp_trial1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxWidthRamp(self, nums: List[int]) -> int:
+
+        stack = []
+        for i in range(len(nums)):
+            
+            if not stack or nums[stack[-1]] > nums[i]:
+                stack.append(i)
+
+
+        max_width = float('-inf')
+        for i in range(len(nums)-1, -1, -1):
+            while stack and nums[i] >= nums[stack[-1]]:
+                max_width = max(max_width, i - stack[-1])
+                stack.pop()
+
+        
+        return max_width
+
+
+        
